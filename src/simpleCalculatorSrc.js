41568407efb035883d36d2f980cf733a002465e4
maza455 <same7nte@gmail.com>
2020-03-09 17:08:42 +0200

    initialised git repo

---
+++ src/simpleCalculatorSrc.js
@@ -0,0 +1,32 @@
+function add(num1, num2){
+    return num1 + num2
+}
+
+function add(){            
+
+    let sum = 0;
+    for( i=0; i<arguments.length; i++){   
+        sum += arguments[i];
+    }
+
+    return sum;
+};
+
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function multiply(){
+
+    let product = 1;
+    for( i=0; i<arguments.length; i++){
+        product *= arguments[i];
+    }
+
+    return product;
+};
+
+console.log(add, multiply);         
+
+module.exports = { add, multiply }         
