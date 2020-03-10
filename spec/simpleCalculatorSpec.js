c28e9870458efb346fd31397475a8ec6a0b679fe
maza455 <same7nte@gmail.com>
2020-03-10 10:33:50 +0200

    last try

---
+++ spec/simpleCalculatorSpec.js
@@ -0,0 +1,32 @@
+const { add, multiply } = require('../../src/simpleCalculatorSrc')
+
+describe("A simple calculator that adds", function() {
+
+  it("should be able to add two numbers", function() {
+    expect(add(1,2)).toEqual(3);
+  });
+
+   it("should be able to add two negative numbers", function() {
+    expect(add(-1,-1)).toEqual(-2);
+  });
+  
+  it("should be able to add more numbers", function() {
+    expect(add(1,2,3,4,5)).toEqual(15);
+  });
+});
+
+//multiply numbers
+describe("A simple calculator that multiplies", function() {
+
+  it("should be able to multiply two numbers", function() {
+    expect(multiply(1,3)).toEqual(3)
+  });
+
+  it("should be able to multiply positive and negative numbers", function() {
+    expect(multiply(-1,3)).toEqual(-3)
+  });
+  
+  it("should be able to multiply more numbers", function() {
+    expect(multiply(1,2,3,4,5)).toEqual(120)
+  });
+});
